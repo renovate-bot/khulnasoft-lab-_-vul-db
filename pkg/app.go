7a274c54323b265bab732cedb2c43c0a20ccf03b
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2023-08-02 08:48:39 +0600

    Create app.go

---
+++ pkg/app.go
@@ -0,0 +1,53 @@
+package pkg
+
+import (
+	"time"
+
+	"github.com/khulnasoft-labs/vulcheck-db/pkg/utils"
+
+	"github.com/khulnasoft-labs/vulcheck-db/pkg/vulnsrc"
+	"github.com/urfave/cli"
+)
+
+type AppConfig struct{}
+
+func (ac *AppConfig) NewApp(version string) *cli.App {
+	app := cli.NewApp()
+	app.Name = "trivy-db"
+	app.Version = version
+	app.Usage = "Trivy DB builder"
+
+	app.Commands = []cli.Command{
+		{
+			Name:   "build",
+			Usage:  "build a database file",
+			Action: build,
+			Flags: []cli.Flag{
+				cli.StringSliceFlag{
+					Name:  "only-update",
+					Usage: "update db only specified distribution",
+					Value: func() *cli.StringSlice {
+						var targets cli.StringSlice
+						for _, v := range vulnsrc.All {
+							targets = append(targets, string(v.Name()))
+						}
+						return &targets
+					}(),
+				},
+				cli.StringFlag{
+					Name:  "cache-dir",
+					Usage: "cache directory path",
+					Value: utils.CacheDir(),
+				},
+				cli.DurationFlag{
+					Name:   "update-interval",
+					Usage:  "update interval",
+					Value:  24 * time.Hour,
+					EnvVar: "UPDATE_INTERVAL",
+				},
+			},
+		},
+	}
+
+	return app
+}
