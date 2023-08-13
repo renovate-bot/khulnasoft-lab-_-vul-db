4a3aae08756272d7d8959d79daa816067c690e97
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2023-08-13 18:21:36 +0000

    fix

--- pkg/vulnsrc/alma/alma.go
+++ pkg/vulnsrc/alma/alma.go
@@ -139,7 +139,7 @@ func (vs *VulnSrc) commit(tx *bolt.Tx, platformName string, errata []Erratum) er
 			}
 
 			// We need to work around this issue for now.
-			// https://github.com/aquasecurity/fanal/issues/186#issuecomment-931523102
+			// https://github.com/khulnasoft-lab/fanal/issues/186#issuecomment-931523102
 			advisories := map[string]types.Advisory{}
 
 			cveID := ref.ID
